a71570bbab73d2b1f997218425fffe4e03165d3e
Andrii Dmytrenko <andrii@fetlife.com>
2023-04-04 11:02:11 +0100

    Fix monkey patch

--- lib/polymorphic_integer_type/belongs_to_polymorphic_association_extension.rb
+++ lib/polymorphic_integer_type/belongs_to_polymorphic_association_extension.rb
@@ -1,10 +1,20 @@
 module ActiveRecord
   module Associations
+    if Rails::VERSION::MAJOR > 6
+      raise "This extension is not compatible with Rails > 6"
+    end
+
     class BelongsToPolymorphicAssociation < BelongsToAssociation
       private def replace_keys(record, force: false)
         super
 
-        target_type = record ? record.class.base_class : nil
+        target_type = if record
+          if owner.class.ancestors.include?(PolymorphicIntegerType::Extensions)
+            record.class.base_class
+          else
+            record.class.polymorphic_name
+          end
+        end
 
         if force || owner[reflection.foreign_type] != target_type
           owner[reflection.foreign_type] = target_type
